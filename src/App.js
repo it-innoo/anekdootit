62c79b46d90ef263c9e7152cb5e6e76daedb4384
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-26 14:59:45 +0300

    Step 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React, { useState } from 'react'
 
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(
+    Array(anecdotes.length).fill(0)
+  )
+
+  const voteFor = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVote(copy)
+  }
+
 
   return (
-    <div>
+    < div >
       <p>
         {anecdotes[selected]}
       </p>
+      <p>
+        has {votes[selected]} votes
+      </p>
 
+      <button onClick={voteFor}>Vote</button>
       <button onClick={() => setSelected(
         Math.floor(Math.random() * anecdotes.length))}>
         Next
       </button>
-    </div>
+    </div >
   )
 }
 

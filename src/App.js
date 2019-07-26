4a0dc65f7de339439dd1585858ba8e39f955d239
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-26 14:40:53 +0300

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        {anecdotes[selected]}
+      </p>
+
+      <button onClick={() => setSelected(
+        Math.floor(Math.random() * anecdotes.length))}>
+        Next
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
